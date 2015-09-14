566eb1a54cc9fcfa257381720bb77d2129272805
chaeplin <chaeplin@gmail.com>
2015-09-15 07:38:08 +0900

    add sleeping

--- Xiaomi_scale_scan.py
+++ Xiaomi_scale_scan.py
@@ -8,6 +8,7 @@
 
 import blescan
 import sys
+import time
 
 import bluetooth._bluetooth as bluez
 
@@ -18,7 +19,7 @@ try:
 
 except:
 	print "error accessing bluetooth device..."
-    	sys.exit(1)
+	sys.exit(1)
 
 blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
@@ -26,20 +27,27 @@ blescan.hci_enable_le_scan(sock)
 try:
 	while True:
 		returnedList = blescan.parse_events(sock, 1)
-	        if len(returnedList) > 0:
+		if len(returnedList) > 0:
 			(mac, uuid, major, minor, txpower, rssi) = returnedList[0].split(',', 6)
 			# change mac and uuid
 			if mac == '88:0f:10:83:ca:5c' and uuid[0:22] == '01880f1083ca5c0d161d18':
 				measunit = uuid[22:24]	
-                		measured = int((uuid[26:28] + uuid[24:26]), 16) * 0.01
-	   
+				measured = int((uuid[26:28] + uuid[24:26]), 16) * 0.01
+
+				unit = ''
+
 				if measunit == '03' or measunit == 'a3' : unit = 'lbs' 
 				if measunit == '12' or measunit == 'b2' : unit = 'jin'
 				if measunit == '22' or measunit == 'a2' : unit = 'Kg' ;  measured = measured / 2
 					
-				print("measured : %s %s" % (measured, unit))
+				if unit:
+					print("measured : %s %s" % (measured, unit))
+				else:
+					print 'scale is sleeping'
+
+		time.sleep(2)
 				
-				# to compare previous measurement , use major and minor ( should be time of measurement)
+		# to compare previous measurement , use major and minor ( should be time of measurement)
 	
 except KeyboardInterrupt:
-       sys.exit(1)
+		sys.exit(1)
